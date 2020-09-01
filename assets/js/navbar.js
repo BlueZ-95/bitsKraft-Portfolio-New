eba0dbf117817323da4de37a8f5c6df0c6eef70c
BlueZ <62077144+BlueZ-95@users.noreply.github.com>
2020-09-01 20:42:13 +0530

    update

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,4 +1,15 @@
 function toggleNavbar(collapseID) {
   document.getElementById(collapseID).classList.toggle("hidden");
   document.getElementById(collapseID).classList.toggle("block");
+
+  animateLinks();
+}
+
+function animateLinks() {
+  var navBar = document.getElementById("navLinks");
+  var navLinks = navBar.getElementsByTagName("li");
+
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.toggle("scaleFonts");
+  }
 }
